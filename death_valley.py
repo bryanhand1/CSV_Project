641aa333b915aad1b8404ac1e63f2e652b26ed13
Bryan Hand <bryan_hand1@baylor.edu>
2021-03-02 15:51:50 -0600

    Finish Death Valley

--- death_valley.py
+++ death_valley.py
@@ -7,7 +7,7 @@
 import csv
 from datetime import datetime
 
-open_file = open("sitka_weather_2018_simple.csv", "r")
+open_file = open("death_valley_2018_simple.csv", "r")
 csv_file = csv.reader(open_file, delimiter=",")
 
 header_row = next(csv_file)
@@ -18,18 +18,25 @@ header_row = next(csv_file)
 for index, column_header in enumerate(header_row):
     print("Index: ", index, "Column Name: ", column_header)
 
+
 highs = []
 dates = []
 lows = []
 
 for row in csv_file:
-    highs.append(int(row[5]))
-    lows.append(int(row[6]))
-    converted_date = datetime.strptime(row[2], "%Y-%m-%d")
-    dates.append(converted_date)
+    try:
+        high = int(row[4])
+        low = int(row[5])
+        converted_date = datetime.strptime(row[2], "%Y-%m-%d")
+    except ValueError:
+        print(f"missing data for {converted_date}")
+    else:
+        highs.append(high)
+        lows.append(low)
+        dates.append(converted_date)
 
 
-print(dates)
+# print(dates)
 
 # print(highs)
 # plot highs on a chart
@@ -49,13 +56,5 @@ plt.title("Daily High and Low Temperatures - 2018", fontsize=16)
 plt.xlabel("", fontsize=12)
 plt.ylabel("Temperature (F)", fontsize=12)
 plt.tick_params(axis="both", labelsize=12)
-
-plt.show()
-
-
-fig2, a = plt.subplots(2)
-
-a[0].plot(dates, highs, c="red")
-a[1].plot(dates, lows, c="blue")
 
 plt.show()
